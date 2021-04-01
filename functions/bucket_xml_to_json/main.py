24f81dcac6dc2453526d8fad83f5c11e007a80bf
SpringPie <bente.vandepol@gmail.com>
2021-04-01 13:37:26 +0200

    Making sure that logging on production does not have sensitive info DAT-6148 [skip ci]

--- functions/bucket_xml_to_json/main.py
+++ functions/bucket_xml_to_json/main.py
@@ -1,14 +1,27 @@
 import logging
+
+from config import DEBUG_LOGGING
+
 import statusprocessor
 
 logging.basicConfig(level=logging.INFO)
 
 
+def log(debug_message, normal_message):
+    if DEBUG_LOGGING:
+        logging.info(debug_message)
+    else:
+        logging.info(normal_message)
+
+
 def status_to_json(data, context):
     # Extract subscription from subscription string
     try:
-        bucket = data['bucket']
-        logging.info(f'A file has been uploaded to bucket {bucket}')
+        bucket = data["bucket"]
+        log(
+            f"A file has been uploaded to bucket {bucket} with context {context}",
+            f"A file has been uploaded to bucket {bucket}",
+        )
 
         processed = statusprocessor.process(data, context)
         if processed is False:
@@ -17,10 +30,10 @@ def status_to_json(data, context):
             logging.info("XML file is processed")
 
     except Exception as e:
-        logging.info('Extract of subscription failed')
+        logging.info("Extract of subscription failed")
         logging.debug(e)
         raise e
 
     # Returning any 2xx status indicates successful receipt of the message.
     # 204: no content, delivery successfull, no further actions needed
-    return 'OK', 204
+    return "OK", 204
